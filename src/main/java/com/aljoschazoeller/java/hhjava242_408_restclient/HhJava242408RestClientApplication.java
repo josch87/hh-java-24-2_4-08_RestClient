c1d8ec0146a4b6db3149110bd9bb82b8f08e7171
Aljoscha Zöller <mail@aljoschazoeller.com>
2024-05-15 11:11:52 +0200

    Initial commit

---
+++ src/main/java/com/aljoschazoeller/java/hhjava242_408_restclient/HhJava242408RestClientApplication.java
@@ -0,0 +1,13 @@
+package com.aljoschazoeller.java.hhjava242_408_restclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HhJava242408RestClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HhJava242408RestClientApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/aljoschazoeller/java/hhjava242_408_restclient/HhJava242408RestClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aljoschazoeller.java.hhjava242_408_restclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HhJava242408RestClientApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
